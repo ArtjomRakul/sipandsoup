4346eda76f810dcd0c3f329242d4156db14985ad
Ilya <116214643+IlyaPoloz@users.noreply.github.com>
2025-05-05 15:38:36 +0300

    #test2 fix

--- assets/js/partials.js
+++ assets/js/partials.js
@@ -1,16 +1,20 @@
 function loadPartial(id, url) {
   fetch(url)
-    .then(response => response.text())
-    .then(html => { document.getElementById(id).innerHTML = html; });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Failed to load ${url}: ${response.statusText}`);
+      }
+      return response.text();
+    })
+    .then(html => { document.getElementById(id).innerHTML = html; })
+    .catch(error => console.error(error));
 }
-document.addEventListener("DOMContentLoaded", function() {
- /* const isGitHubPages = window.location.hostname === "artjomrakul.github.io";
-  const base = document.createElement("base");
-  base.href = isGitHubPages ? "/sipandsoup.github.io/" : "/";
-  document.head.prepend(base); */
 
+document.addEventListener("DOMContentLoaded", function() {
   const isRecipe = window.location.pathname.includes('/recipes/');
-  const partialsPrefix = isRecipe ? '../partials/' : 'partials/';
+  const isRoot = window.location.pathname === '/' || window.location.pathname.endsWith('/index.html');
+  const partialsPrefix = isRecipe ? '../partials/' : isRoot ? 'partials/' : './partials/';
+  
   loadPartial("navbar", partialsPrefix + "navbar.html");
   loadPartial("footer", partialsPrefix + "footer.html");
 });
