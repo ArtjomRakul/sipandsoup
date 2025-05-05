d17e80521077711184af1178353e8a1f6dd6dab5
Ilya <116214643+IlyaPoloz@users.noreply.github.com>
2025-05-05 15:33:28 +0300

    fix urls

--- assets/js/partials.js
+++ assets/js/partials.js
@@ -4,6 +4,11 @@ function loadPartial(id, url) {
     .then(html => { document.getElementById(id).innerHTML = html; });
 }
 document.addEventListener("DOMContentLoaded", function() {
+  const isGitHubPages = window.location.hostname === "artjomrakul.github.io";
+  const base = document.createElement("base");
+  base.href = isGitHubPages ? "/sipandsoup.github.io/" : "/";
+  document.head.prepend(base);
+
   const isRecipe = window.location.pathname.includes('/recipes/');
   const partialsPrefix = isRecipe ? '../partials/' : 'partials/';
   loadPartial("navbar", partialsPrefix + "navbar.html");
